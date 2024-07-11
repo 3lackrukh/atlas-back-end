df7548bdde0ee08f3d65cf9a96a3d8ea21f74e95
3lackrukh <nathan.rhys@atlasschool.com>
2024-07-11 16:24:51 -0500

    1-export_to_CSV.py based on 0-gather_data_from_an_API.py. creates a csv file. retrieves data and writes to csv

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+""" This defines the module for exporting employee task data to CSV"""
+import csv
+import requests
+import sys
+
+
+def get_employee_todo(employee_id):
+    """ This method reports an employee's todo list progress in CSV format"""
+
+    # check that employee_id is an integer
+    if not isinstance(employee_id, int):
+        raise TypeError("employee_id must be an integer")
+
+    # base URL dor the API
+    base_url = 'https://jsonplaceholder.typicode.com'
+
+    # fetch todos for the given employee
+    todo_url = f'{base_url}/todos?userId={employee_id}'
+    user_url = f'{base_url}/users?id={employee_id}'
+    response1 = requests.get(todo_url)
+    response2 = requests.get(user_url)
+
+    if response1.status_code != 200:
+        print(f'Failed to fetch todos for employee ID {employee_id}.')
+        return
+
+    if response2.status_code != 200:
+        print(f'Failed to fetch name for employee ID {employee_id}.')
+        return
+
+    # retrieve todos and employee name
+    todos = response1.json()
+    emp_name = response2.json()[0]['name']
+
+    # construct CSV file name
+    file_name = f'{employee_id}.csv'
+
+    # create and write formatted data to file
+    with open(file_name, mode='w', newline='') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for task in todos:
+            writer.writerow([
+                employee_id, emp_name,
+                task['completed'], task['title']])
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Usage: python3 0-gather_data_from_an_API.py EMPLOYEE_ID")
+    else:
+        employee_id = int(sys.argv[1])
+        get_employee_todo(employee_id)
